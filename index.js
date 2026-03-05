691eb918ec3149b09d7c9cc934c1c75310c99f5b
wornwheel-maker <wornwheel@gmail.com>
2026-03-05 10:06:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const TelegramBot = require('8663683179:AAEETa3tYsbhhxIq-jB_IEoEggC3v8NUNZc');
+const TelegramBot = require('node-telegram-bot-api'); // Первая строка должна быть именно такой!
 
-const token = "PASTE_YOUR_BOT_TOKEN_HERE";
+const token = '8663683179:AAEETa3tYsbhhxIq-jB_IEoEggC3v8NUNZc'; // Токен только здесь
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "🚀 SafeTrade Bot запущен!");
+  bot.sendMessage(msg.chat.id, "🚀 SafeTrade Bot работает!");
 });
+
+console.log("Бот запущен...");
